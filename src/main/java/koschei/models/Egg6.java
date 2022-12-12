4028f7b554a281a656d85f69b6a4a06fa3a5c114
Demonept <66496493+Demonept@users.noreply.github.com>
2022-12-12 15:28:54 +0300

    Созданы все необходимые связи и получена нужная фраза.

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,17 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+@Component
 public class Egg6 {
+    private Deth8 death;
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + death.toString();
+    }
+    @Autowired
+    public void setOcean(Deth8 deth8) {
+        this.death = deth8;
     }
 }
